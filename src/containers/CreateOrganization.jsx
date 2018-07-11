716f7e60ce4d13cd717b412e50df929a9688b7e2
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2018-07-11 15:40:43 -0400

    Move Route's old onEnter into component lifecycle.

--- src/containers/CreateOrganization.jsx
+++ src/containers/CreateOrganization.jsx
@@ -42,6 +42,11 @@ class CreateOrganization extends React.Component {
   formSchema = yup.object({
     name: yup.string().required()
   })
+
+  componentWillMount() {
+    this.props.onEnter()
+  }
+
   renderInvalid() {
     return (
       <div>
@@ -133,7 +138,8 @@ CreateOrganization.propTypes = {
   mutations: PropTypes.object,
   history: PropTypes.object,
   userData: PropTypes.object,
-  inviteData: PropTypes.object
+  inviteData: PropTypes.object,
+  onEnter: PropTypes.func
 }
 
 const mapMutationsToProps = () => ({
